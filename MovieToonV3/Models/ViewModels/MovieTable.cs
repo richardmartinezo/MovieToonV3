e000b2ffd21aa484035de116dcfc2f1df3171585
Nelson Martinez <nelson.martinez@care.com>
2021-10-14 12:12:31 -0500

    done?

---
+++ MovieToonV3/Models/ViewModels/MovieTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieToonV3.Models.ViewModels
+{
+    public class MovieTable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Nullable<double> RentPrice { get; set; }
+    }
+}
